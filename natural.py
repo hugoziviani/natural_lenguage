c3b95bbe94559eee52bd4e1ce269d372bfc8a9e7
Hugo Ziviani <hugoziviani@hotmail.com>
2020-09-29 09:49:28 -0300

    funcinando n_gram=2

--- natural.py
+++ natural.py
@@ -7,29 +7,28 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 from heapq import nlargest
 from string import punctuation
+from collections import Counter
+from itertools import zip_longest
 
 
+def extract_ngrams(data, num):
+    n_grams = ngrams(nltk.word_tokenize(data), num)
+    return [ ' '.join(grams) for grams in n_grams]
 
-
-def raw_text_frequency_and_vocab(text, vocabulary):
+def frequency_by_vocab(text, vocabulary, n_grams):
     tokenizer = RegexpTokenizer(r'\w+')
     words = tokenizer.tokenize(text.lower())
-    
-    # words = word_tokenize(text.lower())
-    stopwords_table = set(list(punctuation))
-    list_to_frequency=list()
-    for wd in words:
-        if wd not in stopwords_table:
-            list_to_frequency.append(wd)
-            if wd not in vocabulary:
-                vocabulary.append(wd)
-    frequency = FreqDist(list_to_frequency)
-    return frequency
+    n_text = ' '.join(words)
+    n_grams_text = extract_ngrams(n_text, n_grams)
 
-def clean_text_frequency_and_vocab(text, vocabulary):
-    frequency = FreqDist(text)
-    return frequency
-
+    result = list()
+    for word in vocabulary:
+        if word in n_grams_text:
+            result.append(n_text.count(word))
+        else:
+            result.append(0)
+    return result
+         
 def without_stop_words(text, vocabulary):
     wrds = word_tokenize(text.lower())
     words_free=list()
@@ -78,43 +77,17 @@ def main():
     text2 = "É fácil escrever código. Difícil é escrever código que funcione."
 
     vocabulary = list()
-
-    frequency1 = raw_text_frequency_and_vocab(text1, vocabulary)
-    frequency2 = raw_text_frequency_and_vocab(text2, vocabulary)
-    relation1 = vocabulary_relation(frequency1, vocabulary)
-    relation2 = vocabulary_relation(frequency2, vocabulary)
-
+    vocabulary_grams = list()
+    n_grams = 2
+    
+    f1 = get_ngrams(text1, n_grams, vocabulary_grams)
+    f2 = get_ngrams(text2, n_grams, vocabulary_grams)
     
 
-    vocabulary_grams = vocabulary_in_n_grams(vocabulary, 2)
+    l = frequency_by_vocab(text2, vocabulary_grams, n_grams)
+    print(l)
     
-    frequency3 = get_ngrams(text1, 2, vocabulary_grams)
-    frequency4 = get_ngrams(text2, 2, vocabulary_grams)
-    relation3 = vocabulary_relation(frequency3, vocabulary_grams)
-    relation4 = vocabulary_relation(frequency4, vocabulary_grams)
-
-    print(vocabulary_grams)
-    print(vocabulary)
-    # frequency1.tabulate()
-    # frequency2.tabulate()
-    # print(relation1)
-    # print(relation2)
-    
-    # somar o vocabulario e depois passar a tabulação de 2 em 2 elementos
-    
-    frequency3.tabulate()
-    frequency4.tabulate()
-    print(relation3)
-    print(relation4)
+        
 
 if __name__ == "__main__":
     main()
-
-
-# O código mantém a órdem das palavras a medida que vai encontrando-as no texto.
-# Decidi fazer assim, pois foi o que entendi da especificação, mas poderia facilmente
-# ordená-las de forma alfabética.
-# O código tem duas funções, uma realiza a separação das palavras sem ignorar as stop-words,
-# já a outra realiza a seleção do vocabuário e recorrências, ignorando-as.
-# Ao pesquisar, busquei uma maneira que fosse mais otmizada, caso o vocabulário crescece de 
-# maneira exponencial, assim aproveitei da modularidade do python para tais funções.
